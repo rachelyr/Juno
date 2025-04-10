ddbc4833372d762a535fde256ff039884a72000e
rachelyr <rachel.yorke225@gmail.com>
2025-04-10 20:08:06 +0530

    Initialized front end
    Basis for front end coded

---
+++ client/src/app/dashboardWrap.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import React, { useEffect } from 'react'
+import Navbar from '@/app/(components)/Navbar';
+import Sidebar from '@/app/(components)/Sidebar';
+import StoreProvider, { useAppSelector } from './redux';
+
+const DashboardLayout= ({children}: {children: React.ReactNode}) => {
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed,  {/*gives us our global store for sidebar*/}
+  );
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  //adding dark mode class to root element if true
+  useEffect(() => {
+    if(!isDarkMode){
+      document.documentElement.classList.add("dark");
+    } else{
+      document.documentElement.classList.remove("dark");
+    }
+  }, [isDarkMode])
+
+  return (
+    <div className='flex min-h-screen w-full bg-gray-50 text-gray-900'>
+      {/*Sidebar*/}
+      <Sidebar/>
+      <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg ${ isSidebarCollapsed ? "" : "md:pl-64"}`}>
+        {/*NAVBAR*/}
+        <Navbar/>
+        {children}
+      </main>
+
+    </div>
+  )
+}
+
+const DashboardWrap = ({children}: {children: React.ReactNode}) => {
+  return (
+    <StoreProvider> {/*so our entire application has access to redux - did this seperately to access global state in dashboard*/}
+      <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
+  )
+}
+
+export default DashboardWrap;
---
+++ client/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+      <main className="flex flex-col justify-center text-2xl min-h-screen p-24 items-center">
+        hi
+      </main>
+  );
+}
